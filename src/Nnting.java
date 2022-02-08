1f33645f6b4147e54e6d9c9a4cf66ec23a9f7ac4
Maxalbinsson <max.albiknsson@elev.ga.ntig.se>
2022-02-08 15:09:18 +0100

    :)

--- src/Nnting.java
+++ src/Nnting.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 class Hangman {
     private static final String[] words = {"buss", "house", "computer", "dog", "spectacular", "swedish"};
     private static final String word = words[(int) (Math.random() * words.length)];
-    private static final String asterisk = new String(new char[word.length()]).replace("\0", "*");
-    private static final int count = 0;
+    private static String asterisk = new String(new char[word.length()]).replace("\0", "*");
+    private static int count = 0;
 
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         while (count(count < 7 && asterisk.contains("*"))) {
-            System.out.println("Guess any letter in the word");
+            System.out.println("Gissa en bokstav");
             System.out.println(asterisk);
             String guess = sc.next();
             hang(guess);
@@ -18,15 +18,63 @@ class Hangman {
         sc.close();
     }
 
+
+
     private static boolean count(boolean b) {
         return b;
     }
 
-    private static void hang(String guess) {
+
+    public static void hang(String guess) {
         String newasterisk = "";
         for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) == guess.charAt(0)) {
+                newasterisk += guess.charAt(0);
+            } else if (asterisk.charAt(i) != '*') {
+                newasterisk += word.charAt(i);
+            } else {
+                newasterisk += "*";
+            }
+        }
+        if (asterisk.equals(newasterisk)) {
+            count++;
+            hangmanImage();
+        } else {
+            asterisk = newasterisk;
+        }
+        if (asterisk.equals(word)) {
+            System.out.println("Rätt svar, du van! Ordet var" + word);
+        }
+
+    }
+
+
+    public static void hangmanImage() {
+        if (count == 1) {
+        System.out.print("fel bokstav, försök igen");
+        System.out.print();
+        System.out.print();
+        System.out.print();
+        System.out.print();
+        System.out.print("____I____");
+        }
+
+
+        if (count == 2) {
+        System.out.print("fel bokstav, försök igen");
+        System.out.print();
+        System.out.print();
+        System.out.print();
+
+
+
+
         }
     }
+
+
+
+
 }
 
 
